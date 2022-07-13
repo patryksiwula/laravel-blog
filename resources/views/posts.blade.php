3590e713696d3dadce9949d8f77d7312c64ac5c2
Patryk Siwula <patryk.siw@gmail.com>
2022-07-13 16:46:11 +0200

    Add fillable property to Post model, change blade variable arrays to object properties

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,10 +6,10 @@
 
 @foreach ($posts as $post)
 	<h2>
-		<a href="/posts/{{ $post['id'] }}">{{ $post['title'] }}</a>
+		<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
 	</h2>
 
-	<p>{{ $post['content'] }}</p>
+	<p>{{ $post->content }}</p>
 @endforeach
 
 @endsection
