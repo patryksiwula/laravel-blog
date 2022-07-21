24a1d8d8b2b80c1ce349c16a9bf420bcee8df41e
Patryk Siwula <patryk.siw@gmail.com>
2022-07-21 20:15:41 +0200

    Add viewAny method to user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,6 +9,17 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+	/**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->is_admin;
+    }
+
     /**
      * Determine whether the user can view the model.
      *
