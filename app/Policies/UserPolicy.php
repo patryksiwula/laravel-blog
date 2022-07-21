8e3100bc67fde7bece93ccd4b73c363fca8cf841
Patryk Siwula <patryk.siw@gmail.com>
2022-07-21 20:13:14 +0200

    Add user policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, User $model)
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, User $targetUser)
+    {
+        return $user->is_admin || (auth()->check() && $user->id == $targetUser->id);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, User $targetUser)
+    {
+        return $user->is_admin;
+    }
+}
