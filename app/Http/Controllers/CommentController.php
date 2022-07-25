c1b7758dcba11458b1b6e5f8257f4787142b0220
Patryk Siwula <patryk.siw@gmail.com>
2022-07-25 19:51:52 +0200

    Add post parameter to controller's methods arguments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,30 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\Comment;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {	
-	public function edit(Comment $comment): View
+	public function edit(Post $post, Comment $comment): View
 	{
 		return view('posts.comments.edit', [
+			'post' => $post,
 			'comment' => $comment
 		]);
 	}
 
-    public function store(Request $request): void
+    public function store(Request $request, Post $post): void
 	{
 		# code...
 	}
 
-	public function update(Request $request, Comment $comment): void
+	public function update(Request $request, Post $post, Comment $comment): void
 	{
 		# code...
 	}
 
-	public function destroy(Comment $comment): void
+	public function destroy(Post $post, Comment $comment): void
 	{
 		# code...
 	}
