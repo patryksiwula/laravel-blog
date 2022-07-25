a88d0d6f86f37977d45975b764ee3606ee4bd230
Patryk Siwula <patryk.siw@gmail.com>
2022-07-25 22:43:03 +0200

    Add update and delete methods to comment controller, add authorization

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Actions\CreateComment;
-use App\Http\Requests\StoreCommentRequest;
+use App\Http\Requests\CommentRequest;
 use App\Models\Post;
 use App\Models\Comment;
 use Illuminate\Contracts\View\View;
@@ -14,13 +14,15 @@ class CommentController extends Controller
 {	
 	public function edit(Post $post, Comment $comment): View
 	{
+		$this->authorize('update', $comment);
+
 		return view('posts.comments.edit', [
 			'post' => $post,
 			'comment' => $comment
 		]);
 	}
 
-    public function store(StoreCommentRequest $request, Post $post, CreateComment $createComment): RedirectResponse
+    public function store(CommentRequest $request, Post $post, CreateComment $createComment): RedirectResponse
 	{
 		$createComment->handle(
 			auth()->id(),
@@ -33,13 +35,26 @@ class CommentController extends Controller
 		])->withMessage('action', 'comment_created');
 	}
 
-	public function update(Request $request, Post $post, Comment $comment): void
+	public function update(CommentRequest $request, Post $post, Comment $comment): RedirectResponse
 	{
-		# code...
+		$this->authorize('update', $comment);
+
+		$comment->content = $request->input('comment_content');
+		$comment->save();
+
+		return redirect()->route('posts.show', [
+			'post' => $post
+		])->withMessage('action', 'comment_updated');
 	}
 
-	public function destroy(Post $post, Comment $comment): void
+	public function destroy(Post $post, Comment $comment): RedirectResponse
 	{
-		# code...
+		$this->authorize('delete', $post, $comment);
+
+		$comment->delete();
+
+		return redirect()->route('posts.show', [
+			'post' => $post
+		])->withMessage('action', 'comment_deleted');
 	}
 }
--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -15,12 +15,12 @@ class CommentPolicy
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
-     * @param  \App\Models\Post  $post
+     * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function update(User $user, Comment $comment)
     {
-        return $user->is_admin || (auth()->check() && auth()->id() == $comment->user->id);
+        return $user->is_admin || auth()->id() == $comment->user->id;
     }
 
     /**
@@ -28,12 +28,13 @@ class CommentPolicy
      *
      * @param  \App\Models\User  $user
      * @param  \App\Models\Post  $post
+	 * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function delete(User $user, Post $post, Comment $comment)
     {
         return $user->is_admin
 			|| $comment->user->id == $post->user->id
-			|| (auth()->check() && auth()->id() == $comment->user->id);
+			|| auth()->id() == $comment->user->id;
     }
 }
