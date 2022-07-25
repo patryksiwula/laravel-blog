fa0d21b7778bbc85438b64cdd01f6a5dd3779e89
Patryk Siwula <patryk.siw@gmail.com>
2022-07-25 18:30:09 +0200

    Add comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'user_id',
+		'post_id',
+		'content'
+	];
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function post(): BelongsTo
+	{
+		return $this->belongsTo(Post::class);
+	}
+}
