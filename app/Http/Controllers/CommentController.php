ff3ad39f7f1e18cc8373360c7bec888436b9b25c
Patryk Siwula <patryk.siw@gmail.com>
2022-07-25 20:24:22 +0200

    Add form request for creating comments, add store method to comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\CreateComment;
+use App\Http\Requests\StoreCommentRequest;
 use App\Models\Post;
 use App\Models\Comment;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -17,9 +20,17 @@ class CommentController extends Controller
 		]);
 	}
 
-    public function store(Request $request, Post $post): void
+    public function store(StoreCommentRequest $request, Post $post, CreateComment $createComment): RedirectResponse
 	{
-		# code...
+		$createComment->handle(
+			auth()->id(),
+			$post->id,
+			$request->input('comment_content')
+		);
+
+		return redirect()->route('posts.show', [
+			'post' => $post
+		])->withMessage('action', 'comment_created');
 	}
 
 	public function update(Request $request, Post $post, Comment $comment): void
