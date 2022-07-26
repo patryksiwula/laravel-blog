d2de689d91e89654bc80dd4ec800714aa4135687
Patryk Siwula <patryk.siw@gmail.com>
2022-07-26 17:59:11 +0200

    Add @checked and @disabled directives to checkboxes

--- resources/views/livewire/set-admin.blade.php
+++ resources/views/livewire/set-admin.blade.php
@@ -1,6 +1,7 @@
 <div class="text-gray-900 whitespace-no-wrap">
 	<label class="flex items-center space-x-3 mb-3">
-		<input wire:change="setAdmin(!{{ $user->is_admin }})" {{ $user->is_admin ? 'checked' : '' }} {{ $user->id == Auth::user()->id ? 'disabled' : '' }} type="checkbox" name="checked-demo" class="form-tick
-			appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500 checked:border-transparent focus:outline-none"/>
+		<input wire:change="setAdmin(!{{ $user->is_admin }})" @checked($user->is_admin) @disabled($user->id == Auth::user()->id) type="checkbox"
+			name="checked-demo" class="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-gray-500
+			checked:border-transparent focus:outline-none"/>
 	</label>
 </div>
