8e927f1b0de204450b2ca5201d09d05b8f3042e9
Patryk Siwula <patryk.siw@gmail.com>
2022-07-27 01:28:27 +0200

    Add documentation, add replies() and parent() methods

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
@@ -20,14 +21,44 @@ class Comment extends Model
 		'post_id',
 		'content'
 	];
-
+	
+	/**
+	 * Get the user which the comment belongs to
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function user(): BelongsTo
 	{
 		return $this->belongsTo(User::class);
 	}
-
+	
+	/**
+	 * Get the post which the comment belongs to
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function post(): BelongsTo
 	{
 		return $this->belongsTo(Post::class);
+	}
+	
+	/**
+	 * Get the replies to the comment
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function replies(): HasMany
+	{
+		return $this->hasMany(Comment::class, 'parent_id');
+	}
+	
+	/**
+	 * Get the comment which the reply belongs to
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function parent(): BelongsTo
+	{
+		return $this->belongsTo(Comment::class, 'parent_id');
 	}
 }
