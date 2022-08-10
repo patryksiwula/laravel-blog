8168636fc71a315cbb10db919821161e3ac82571
Patryk Siwula <patryk.siw@gmail.com>
2022-08-10 21:07:19 +0200

    Change name of test method

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
+    public function testHomepageRedirectsToPostList(): void
     {
         $response = $this->get('/');
-
         $response->assertRedirect('/posts');
     }
 }
