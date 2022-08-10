8b4147b58130ccc5fef8d1fb21b259af8d8be603
Patryk Siwula <patryk.siw@gmail.com>
2022-08-10 23:46:26 +0200

    Add unique to $title

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,7 +19,7 @@ class PostFactory extends Factory
      */
     public function definition()
     {
-		$title = $this->faker->sentence();
+		$title = $this->faker->unique()->sentence();
 
         return [
             'title' => $title,
