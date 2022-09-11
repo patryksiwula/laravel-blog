9296d13603a232d21168da5b0ed1cbcf6e305620
Patryk Siwula <patryk.siw@gmail.com>
2022-09-11 13:23:17 +0200

    Add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,14 +20,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	//\App\Models\User::factory(5)->create();
-		//Category::factory(5)->create();
-		//Post::factory(30)->create();
-		Comment::factory(60)->create();
+		User::firstOrCreate(
+			['name' => 'Test Admin'],
+			['email' => 'admin@example.com'],
+			['email_verified_at' => now()],
+			['password' => Hash::make('admin123')],
+			['remember_token' => Str::random(10)],
+			['is_admin' => 1]
+		);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+		User::factory(5)->create();
+
+		Category::create(['name' => 'Category 1', 'slug' => 'category-1']);
+		Category::create(['name' => 'Category 2', 'slug' => 'category-2']);
+		Category::create(['name' => 'Category 3', 'slug' => 'category-3']);
+		Category::create(['name' => 'Category 4', 'slug' => 'category-4']);
+		Category::create(['name' => 'Category 5', 'slug' => 'category-5']);
+
+		Post::factory(30)->create();
+		Comment::factory(60)->create();
     }
 }
